c938ec07f113a96823d56329628cb7f8f8e84ec1
Matias Davezac <8675@holbertonstudents.com>
2024-09-10 09:49:59 -0300

    Guardacion

--- 1-dart_basics/my_project/routes/login.dart
+++ 1-dart_basics/my_project/routes/login.dart
@@ -1,6 +1,14 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:dart_frog_request_logger/dart_frog_request_logger.dart';
 
 Response onRequest(RequestContext context) {
-  // TODO: implement route handler
-  return Response(body: 'This is a new route!');
+  final request = context.request;
+  final headers = request.headers;
+  if (headers.containsKey('Content-Type') &&
+      headers['Content-Type'] == 'application/json') {
+    context.read<RequestLogger>().debug('JSON');
+    return Response(body: 'JSONER');
+  }
+  context.read<RequestLogger>().debug('NOT JSON');
+  return Response(body: 'NO JSONER');
 }
